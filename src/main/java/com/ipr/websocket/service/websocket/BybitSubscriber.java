8df95f6a397a75b7a005c8bffeb1fe31e3083ab9
Alexandr Strukov <sasha_strukov_00@mail.ru>
2025-01-14 18:30:19 +0300

    тестого кафку прикрутили

--- src/main/java/com/ipr/websocket/service/websocket/BybitSubscriber.java
+++ src/main/java/com/ipr/websocket/service/websocket/BybitSubscriber.java
@@ -9,9 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.client.WebSocketClient;
 
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -23,44 +20,22 @@ public class BybitSubscriber {
     @Value("${services.bybit.symbol}")
     private final List<String> symbols;
     private final ObjectMapper objectMapper;
-    private int reconnectAttempt = 0;
-    @Value("${services.bybit.reconnect.delay:5000}")
-    private long reconnectDelay;
-    @Value("${services.bybit.reconnect.maxAttempts:1000}")
-    private int maxReconnectAttempts;
-
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     @PostConstruct
     public void subscribeToSymbols() {
         connectToWebSocket(requestTopic());
     }
 
-    public void reconnectToSymbols() {
-        reconnectAttempt++;
-        if (reconnectAttempt > maxReconnectAttempts) {
-            log.error("Превышено максимальное количество попыток переподключения ({})", maxReconnectAttempts);
-            return;
-        }
-
-        log.info("Попытка переподключения №{} через {} мс", reconnectAttempt, reconnectDelay);
-
-        scheduler.schedule(() -> {
-            connectToWebSocket(requestTopic());
-        }, reconnectDelay, TimeUnit.MILLISECONDS);
-    }
 
     private void connectToWebSocket(List<String> topics) {
         webSocketClient.execute(
-                        new WebSocketClientHandler(this, klineServiceWebsocket, topics, objectMapper),
+                        new WebSocketClientHandler(klineServiceWebsocket, topics, objectMapper),
                         "wss://stream.bybit.com/v5/public/linear")
                 .whenComplete((session, throwable) -> {
                     if (throwable != null) {
                         log.error("Ошибка при подключении к WebSocket для symbols {}: {}", symbols, throwable.getMessage());
-                        reconnectToSymbols(); // Повторное подключение в случае ошибки
                     } else {
                         log.info("Подключение к WebSocket успешно установлено для symbols {}", symbols);
-                        reconnectAttempt = 0; // Сброс счетчика при успешном подключении
                     }
                 });
     }
